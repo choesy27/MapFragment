6d9c9a4a536a36c14a4ad6e1dffdef444565a0ea
choesy27 <choesy@suwon.ac.kr>
2020-05-11 16:52:47 +0900

    Map Marker Fragment

---
+++ app/src/main/java/com/example/mapfragmenttest/ui/map/MapActivity.java
@@ -0,0 +1,16 @@
+package com.example.mapfragmenttest.ui.map;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mapfragmenttest.R;
+
+public class MapActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_map);
+
+    }
+}
